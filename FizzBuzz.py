8cc19ea44b852400600314613b9ebfd9f53a9aa9
root <root@LAPTOP-BS4NUUBF.localdomain>
2022-10-14 15:15:42 -0500

    more efficent FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,10 +2,10 @@ for x in range(1,101):
   if x % 3 == 0 and x % 5 ==0:
     print("FizzBuzz")
     continue
-  if x % 3 == 0:
+  elif x % 3 == 0:
     print("Fizz")
     continue
-  if x % 5 == 0:
+  elif x % 5 == 0:
     print("Buzz")
     continue
   else:
